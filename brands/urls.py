8c537359e8bac57c37706a38dfdb1b959735dc16
Marcelo Borges <marcelosbgs@gmail.com>
2025-12-31 19:46:31 -0300

    estrutura html de pesquisa/user

--- brands/urls.py
+++ brands/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('brands/list/', views.BrandListView.as_view(), name='brands_list'),
+    path('brands/list/', views.BrandListView.as_view(), name='brand_list'),
 ]
 
