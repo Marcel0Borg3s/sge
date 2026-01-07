ee596472526393e0100b5022fdccad387bea1df5
Marcelo Borges <marcelosbgs@gmail.com>
2026-01-07 07:06:24 -0300

    Atualização e inclusão do Update em Lists

--- brands/views.py
+++ brands/views.py
@@ -1,6 +1,6 @@
 from django.urls import reverse_lazy
 from django.db.models.query import QuerySet
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, DetailView, UpdateView
 from . import models, forms
 
 
@@ -24,3 +24,16 @@ class BrandCreateView(CreateView):
     template_name = 'brand_create.html'
     form_class = forms.BrandForm
     success_url = reverse_lazy('brand_list')
+
+
+class BrandDetailView(DetailView):
+    model = models.Brand
+    template_name = 'brand_detail.html'
+
+
+class BrandUpdateView(UpdateView):
+    model = models.Brand
+    template_name = 'brand_update.html'
+    form_class = forms.BrandForm
+    success_url = reverse_lazy('brand_list')
+
