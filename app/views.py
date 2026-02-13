517983159296150d336ad8f764483691dca7cb00
Marcelo Borges <marcelosbgs@gmail.com>
2026-02-12 21:06:13 -0300

    #49 correção do home.html, erro na execução

--- app/views.py
+++ app/views.py
@@ -1,11 +1,18 @@
+import json
 from django.shortcuts import render
 from . import metrics
 
 
 def home(request):
     product_metrics = metrics.get_products_metrics()
+    sales_metrics = metrics.get_sales_metrics()
+    daily_sales_data = metrics.get_daily_sales_data()
+    daily_sales_quantity_data = metrics.get_daily_sales_quantity_data()
     
     context = {
-        'product_metrics': product_metrics
+        'product_metrics': product_metrics,
+        'sales_metrics': sales_metrics,
+        'daily_sales_data': json.dumps(daily_sales_data),
+        'daily_sales_quantity_data': json.dumps(daily_sales_quantity_data),
     }
     return render(request, 'home.html', context)
